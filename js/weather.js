7e32c2a80ef4a824ae546155aae3823adbd2f5c9
ChoiByeol <honey333888@naver.com>
2021-09-06 00:10:39 +0900

    feat: weather

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "1b45878018388a95c4f1bca28e9008d3";
+
+function onGeoOk(location) {
+    const lat = location.coords.latitude;
+    const lon = location.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = data.weather[0].main;
+        city.innerText = `${data.name} / ${data.main.temp}°C`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
