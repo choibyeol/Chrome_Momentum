c91e716bab89774ea60a9cc13dae699f55a20ca8
ChoiByeol <honey333888@naver.com>
2021-09-06 02:10:27 +0900

    feat: css 추가

--- js/background.js
+++ js/background.js
@@ -1,10 +1,6 @@
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
-
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const src = `img/${chosenImage}`;
 
-bgImage.src = `img/${chosenImage}`;
-console.log(bgImage);
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage =  `url(../${src})`;
