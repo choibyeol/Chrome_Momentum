87dcac4e4b564d5015df0b4e742b41eec7e1a34e
ChoiByeol <honey333888@naver.com>
2021-09-06 15:11:43 +0900

    refactor: console.log 삭제, 띄어쓰기 수정

--- js/greetings.js
+++ js/greetings.js
@@ -9,7 +9,6 @@ function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log(username);
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
 }
--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,6 @@ function onGeoOk(location) {
     const lat = location.coords.latitude;
     const lon = location.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    console.log(url);
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
